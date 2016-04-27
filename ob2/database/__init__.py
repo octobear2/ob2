6f8d41efbf1ef34d0a2328fd302d3d23310dba45
Roger Chen <roger@rogerhub.com>
2016-04-27 02:03:06 -0700

    [Bug fix] The "RUN AUTOGRADER" button can cause database deadlock
    The request handler for the "RUN AUTOGRADER" button is one function where two
    transactions are used in the same execution frame. Because context manager
    variables stay in the local scope until the function returns, the second cursor
    is instantiated while the first one remains active. In certain circumstances,
    this can cause a permanent deadlock condition where the database becomes
    unavailable for all requests.
    
    In order to address this issue, we must not allow database users to keep a copy
    of APSW's cursor object in their local execution frame. The DbCursor object now
    holds the only reference to the cursor object, so the destructor can be called
    immediately after the transaction is finished.

--- ob2/database/__init__.py
+++ ob2/database/__init__.py
@@ -1,8 +1,13 @@
 import apsw
 import ob2.config as config
+import threading
 from ob2.config.assignment import Assignment
 from ob2.database.virtual import GenericReadOnlyVTModule
 from ob2.util.hooks import apply_filters
+
+# For low-traffic applications, a global Python-level lock is the recommended
+# way to prevent database lock conflicts.
+global_database_lock = threading.Lock()
 
 
 class DbCursor(object):
@@ -10,12 +15,15 @@ class DbCursor(object):
     Creates a database handle with transaction semantics. Usage:
 
         with DbCursor() as c:
-            c.execute("SELECT ... ")
+            c.execute("...")
             c.fetchall()
 
     A DbCursor cannot be used more than once, since we close and flush the connection once the first
     transaction completes. You should try to keep blocking operations (disk or network) out of the
     transaction.
+
+    For daemon threads, you should catch apsw.Error and retry the transaction when it fails. Make
+    sure that your retries are idempotent.
 
     """
     def __init__(self, path=None, read_only=False):
@@ -25,21 +33,53 @@ class DbCursor(object):
             flags = apsw.SQLITE_OPEN_READONLY
         else:
             flags = apsw.SQLITE_OPEN_CREATE | apsw.SQLITE_OPEN_READWRITE
-        self.connection = apsw.Connection(path, flags)
-        self.connection.setbusytimeout(5000)
-        for module in apply_filters("database-vtmodules", [self.get_assignments_vtmodule()]):
-            module.registerWithConnection(self.connection)
+
+        # The global lock is acquired in the constructor, so you must never instantiate a DbCursor
+        # object without actually using it.
+        global_database_lock.acquire()
+
+        try:
+            # The connection setup must be done in the constructor, NOT in __enter__.
+            # If __enter__ raises an exception, then the __exit__ method will also be called.
+            self.connection = apsw.Connection(path, flags)
+            self.connection.setbusytimeout(5000)
+            for module in apply_filters("database-vtmodules", [self.get_assignments_vtmodule()]):
+                module.registerWithConnection(self.connection)
+        except Exception:
+            global_database_lock.release()
+            raise
 
     def __enter__(self):
         self.connection.__enter__()
-        return self.connection.cursor()
+
+        # We must save the cursor as a state variable, so we hold the only reference to it and we
+        # can safely call the destructor for the cursor in __exit__.
+        self.cursor = self.connection.cursor()
+        return self
 
     def __exit__(self, *args):
-        self.connection.__exit__(*args)
-        # We need to explicitly call the destructor in order to make sure locks are freed.
-        # Otherwise, Python is free to delay the destructor until a future point in time, which may
-        # cause deadlock.
-        del self.connection
+        try:
+            self.connection.__exit__(*args)
+
+            # We need to explicitly call the destructor in order to make sure locks are freed.
+            # Otherwise, Python is free to delay the destructor until a future point in time, which may
+            # cause deadlock.
+            del self.cursor
+            del self.connection
+        finally:
+            global_database_lock.release()
+
+    def execute(self, *args):
+        return self.cursor.execute(*args)
+
+    def fetchone(self):
+        return self.cursor.fetchone()
+
+    def fetchall(self):
+        return self.cursor.fetchall()
+
+    def getdescription(self):
+        return self.cursor.getdescription()
 
     _assignments_vtmodule = None
 
