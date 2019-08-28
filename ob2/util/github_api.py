0013444f4c1b548a6ef59331acc8a05538c4da1f
Sam Kumar <samkumar99@gmail.com>
2019-08-28 00:32:07 -0700

    Workaround for bug in github3.py version 0.9.6

--- ob2/util/github_api.py
+++ ob2/util/github_api.py
@@ -85,10 +85,31 @@ def _assign_repo(repo_name, members=[]):
 
     for member in members:
         if member not in collaborators:
-            successfully_added = repo.add_collaborator(member)
-            assert successfully_added, "Unable to add member %s to %s" % (repr(member),
-                                                                          repr(fq_repo_name))
-
+            # Due to a BUG in github3 version 0.9.6, repo.add_collaborator will
+            # return False even if an invitation is sent out. This is because
+            # the library mistakenly expects the HTTP return code from GitHub
+            # to be 204, but it's actually 201 (204 means the user was already
+            # a collaborator). This was fixed in version 1.0.0 of the library,
+            # but we can't upgrade due to other issues introduced in that
+            # version (see the note in requirements.txt).
+            #
+            # So this is the code we WANT to write. If we upgrade to a later
+            # version that fixes this, then we can just write this code (but
+            # see the comment in requirements.txt if you choose to undertake
+            # that! v1.0.0 has additional bigger bugs, which is why I didn't
+            # upgrade):
+            #
+            # successfully_added = repo.add_collaborator(member)
+            # assert successfully_added, "Unable to add member %s to %s" % (repr(member),
+            #                                                               repr(fq_repo_name))
+            #
+            # Until then, I have implemented this TERRIBLE UGLY workaround that
+            # at least gives us correct behavior.
+            assert member is not None, "Trying to add None member as collaborator!"
+            url = repo._build_url('collaborators', member, base_url=repo._api)
+            c = repo._put(url)
+            assert c.status_code == 201 or c.status_code == 204, \
+                "Unable to add member %s to %s" % (repr(member), repr(fq_repo_name))
 
 class GitHubTransactionError(Exception):
     pass
